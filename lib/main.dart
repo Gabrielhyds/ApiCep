1384c6e3885a10f13999b04793b4e26a595b1950
FlutLab.io <>
2022-03-30 23:09:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
-import 'dart:convert';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // Application name
+      title: 'Flutter Stateful Clicker Counter',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // Application theme data, you can set the colors for the application as
+        // you want
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Wikipedia search API'),
+      home: MyHomePage(title: 'Cadastro de clientes.'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -29,171 +31,229 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final searchTextController = TextEditingController();
-  List<WikiSearchEntity> searchList = [];
+  TextEditingController _nome = TextEditingController();
+  TextEditingController _cep = TextEditingController();
+  TextEditingController _rua = TextEditingController();
+  TextEditingController _complemento = TextEditingController();
+  TextEditingController _bairro = TextEditingController();
+  TextEditingController _cidade = TextEditingController();
+  TextEditingController _estado = TextEditingController();
+  TextEditingController _rendaMensal = TextEditingController();
+  TextEditingController _limiteCredito = TextEditingController();
 
-  void _search() {
-    String str = searchTextController.text;
-    RequestService.query(str).then((WikiSearchResponse? response) {
+  void _buscaCEP() async {
+    if (_cep.text.trim().length == 8) {
+      String urlAPI = "https://viacep.com.br/ws/" + _cep.text + "/json/";
+      http.Response response;
+      response = await http.get(urlAPI);
+      Map<String, dynamic> retorno = json.decode(response.body);
+
+      print(retorno["logradouro"].toString());
+      print(retorno["complemento"].toString());
+      print(retorno["bairro"].toString());
+      print(retorno["localidade"].toString());
+      print(retorno["uf"].toString());
+
       setState(() {
-        searchList = response!.query.search;
+        _rua.text = retorno["logradouro"].toString();
+        _complemento.text = retorno["complemento"].toString();
+        _bairro.text = retorno["bairro"].toString();
+        _cidade.text = retorno["localidade"].toString();
+        _estado.text = retorno["uf"].toString();
       });
-    });
+    }
   }
 
-  @override
-  void dispose() {
-    searchTextController.dispose();
-    super.dispose();
+  void _gravar() {
+    _buscaCEP();
+    setState(() {});
+  }
+
+  void _limpar() {
+// É necessário se alterar o Estado da aplicação para poder exibir o resultado por isso o setState
+    setState(() {
+      _nome.text = "";
+      _cep.text = "";
+      _rua.text = "";
+      _complemento.text = "";
+      _bairro.text = "";
+      _cidade.text = "";
+      _estado.text = "";
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Center(child: Text(widget.title)),
       ),
-      body: Container(
-        child: Padding(
-          padding: EdgeInsets.all(10),
+      body: SingleChildScrollView(
+        child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            //mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: searchTextController,
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'TextField',
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      height: 60,
-                      child: OutlinedButton(
-                        onPressed: _search,
-                        child: Text("Search"),
-                      ),
-                    ),
-                  ]
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                    autofocus: true,
+                    maxLength: 50,
+                    controller: _nome,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.person_add),
+                      border: OutlineInputBorder(),
+                      labelText: "Digite seu nome",
+                    )),
               ),
-              SizedBox(
-                height: 10,
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 8,
+                    controller: _cep,
+                    // Conforme inserção do cep, os campos subsequentes serão automaticamente preeenchidos.
+                    onChanged: (inputValue) {
+                      int? cep = int.tryParse(_cep.text);
+
+                      String rua = _rua.text.toString();
+                      String complemento = _complemento.text.toString();
+                      String bairro = _bairro.text.toString();
+                      String cidade = _cidade.text.toString();
+                      String estado = _estado.text.toString();
+
+                      if (cep != 0) {
+                        _rua.text = rua;
+                        _complemento.text = complemento;
+                        _bairro.text = bairro;
+                        _cidade.text = cidade;
+                        _estado.text = estado;
+                      }
+                      ;
+                    },
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.add_location_alt_rounded),
+                      border: OutlineInputBorder(),
+                      labelText: "Digite o CEP",
+                    )),
               ),
-              Expanded(child:
-                SingleChildScrollView(
-                child: ListView.builder(
-                  primary: false,
-                  itemBuilder: (BuildContext context,
-                      int index) => new WikiSearchItemWidget(searchList[index]),
-                  itemCount: searchList.length,
-                  shrinkWrap: true,
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 50,
+                    enabled: false,
+                    controller: _rua,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Digite a Rua",
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 50,
+                    enabled: false,
+                    controller: _complemento,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Digite o Complemento",
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 50,
+                    enabled: false,
+                    controller: _bairro,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Digite o Bairro",
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 50,
+                    enabled: false,
+                    controller: _cidade,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Digite a Cidade",
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 2,
+                    enabled: false,
+                    controller: _estado,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Digite o Estado",
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 11,
+                    controller: _rendaMensal,
+                    // Conforme a renda mensal for mudando o limite de crédito(30%) acompanha.
+                    onChanged: (inputValue) {
+                      double? limite = double.tryParse(_rendaMensal.text);
+                      if (limite != null) _limiteCredito.text = (limite * 0.3).toString();
+                    },
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.money),
+                      border: OutlineInputBorder(),
+                      labelText: "Digite a Renda Mensal",
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                child: TextField(
+                    maxLength: 11,
+                    enabled: false,
+                    controller: _limiteCredito,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.credit_card),
+                      border: OutlineInputBorder(),
+                      labelText: "Limite de crédito",
+                    )),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 5, 20),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      primary: Colors.white,
+                      padding: EdgeInsets.all(20.0),
+                      textStyle: TextStyle(fontSize: 17),
+                    ),
+                    onPressed: () {
+                      _gravar();
+                    },
+                    child: Text('Cadastrar'),
+                  ),
                 ),
-              ),
-              ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 0, 20, 20),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      primary: Colors.white,
+                      padding: EdgeInsets.all(20.0),
+                      textStyle: TextStyle(fontSize: 17),
+                    ),
+                    onPressed: () {
+                      _limpar();
+                    },
+                    child: Text("Limpar"),
+                  ),
+                ),
+              ]),
             ],
           ),
         ),
       ),
     );
   }
-}
-
-class WikiSearchItemWidget extends StatelessWidget {
-  final WikiSearchEntity _entity;
-
-  WikiSearchItemWidget(this._entity);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(_entity.title,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-      subtitle: SingleChildScrollView(
-        child: Html(data: _entity.snippet),
-      ),
-      onTap: () {
-
-      },
-    );
-  }
-}
-
-class RequestService {
-  static Future<WikiSearchResponse?> query(String search) async {
-    var response = await http.get(
-        Uri.parse("https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&srsearch=$search&format=json")
-    );
-    // Check if response is success
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      var map = json.decode(response.body);
-      return WikiSearchResponse.fromJson(map);
-    } else {
-      print("Query failed: ${response.body} (${response.statusCode})");
-      return null;
-    }
-  }
-}
-
-class WikiSearchResponse {
-  String batchComplete;
-  WikiQueryResponse query;
-  WikiSearchResponse({required this.batchComplete, required this.query});
-
-  factory WikiSearchResponse.fromJson(Map<String, dynamic> json) => WikiSearchResponse(
-      batchComplete: json["batchcomplete"],
-      query: WikiQueryResponse.fromJson(json["query"])
-  );
-}
-
-class WikiQueryResponse {
-  List<WikiSearchEntity> search;
-
-  WikiQueryResponse({required this.search});
-
-  factory WikiQueryResponse.fromJson(Map<String, dynamic> json) {
-    List<dynamic> resultList = json['search'];
-    List<WikiSearchEntity> search = resultList.map((dynamic value) =>
-        WikiSearchEntity.fromJson(value))
-        .toList(growable: false);
-    return WikiQueryResponse(
-        search: search
-    );
-  }
-}
-
-class WikiSearchEntity {
-  int ns;
-  String title;
-  int pageId;
-  int size;
-  int wordCount;
-  String snippet;
-  String timestamp;
-  WikiSearchEntity({required this.ns, required this.title,
-    required this.pageId, required this.size, required this.wordCount,
-    required this.snippet, required this.timestamp});
-
-  factory WikiSearchEntity.fromJson(Map<String, dynamic> json) => WikiSearchEntity(
-    ns: json["ns"],
-    title: json["title"],
-    pageId: json["pageid"],
-    size: json["size"],
-    wordCount: json["wordcount"],
-    snippet: json["snippet"],
-    timestamp: json["timestamp"]
-  );
 }
